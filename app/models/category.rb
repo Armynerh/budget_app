fed5bb52c2e20756743a07f5692a079c7c1d073d
Armynerh <aimenah@outlook.com>
2023-12-05 19:38:38 -0500

    add db files

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ApplicationRecord
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :budget_transactions
+
+  validates :name, presence: true, uniqueness: true
+  validates :icon, presence: true
+end
