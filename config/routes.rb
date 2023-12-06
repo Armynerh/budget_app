17c2c29de5ebd956159aaa580fb6f08ebc8919a0
Armynerh <aimenah@outlook.com>
2023-12-05 19:39:21 -0500

    modify config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Splash screen
+  root 'splash#index'
+
+  # Categories and Budget Transactions
+  resources :categories, only: [:index] do
+    resources :budget_transactions, only: [:new, :create]
+  end
 end
