db07a21a212c7b7607e0ff834005053dcd9c5e74
Armynerh <aimenah@outlook.com>
2023-12-05 19:47:30 -0500

    modify models to add validations

--- app/models/budget_transaction.rb
+++ app/models/budget_transaction.rb
@@ -3,6 +3,6 @@ class BudgetTransaction < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :name, presence: true
-  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :amount, presence: true
   validates :category, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :budget_transactions, foreign_key: 'author_id'
-  has_and_belongs_to_many :categories
+  has_many :budget_transactions, dependent: :destroy, foreign_key: 'author_id'
+  has_and_belongs_to_many :categories, dependent: :destroy
 
-  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
